e74147b9da65ed5a263a4ee02ebad67a54ddb460
bangsv <zahar.yury@mail.ru>
2026-01-11 19:43:42 +0300

    ph_4104

--- packages/my_rule/normalization_formulas/Powershell/Powershell_log_4104/tests/norm_1.js
+++ packages/my_rule/normalization_formulas/Powershell/Powershell_log_4104/tests/norm_1.js
@@ -1,1 +1,11 @@
-{}
+{
+    "action": "create",
+    "datafield1": "C:\\Users\\alonzo.spire\\Downloads\\powerview.ps1",
+    "object": "command",
+    "object.account.domain": "forela.local",
+    "object.account.fullname": "Forela-Wkstn001.forela.local",
+    "object.account.privileges": "S-1-5-21-3239415629-1862073780-2394361899-1104",
+    "src.host": "forela-wkstn001",
+    "status": "success",
+    "time": "2026-01-11T19:20:09.000Z"
+}
